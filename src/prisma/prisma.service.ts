60c96ee19e580c629189744f488ef14a766862e4
Keval <kevalvadhiya101@gmail.com>
2023-02-23 17:54:07 +0530

    update DB_URL

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -8,7 +8,7 @@ export class PrismaService extends PrismaClient {
     super({
       datasources: {
         db: {
-          url: config.get('DATABASE_URL'),
+          url: "mysql://root:ZMnjSyUpHPWojHfcH0V9@containers-us-west-189.railway.app:7098/railway",
         },
       },
     });
