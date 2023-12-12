3657664a8de0a3392e1dde8cb2f27eba0f2f4373
Keval <kevalvadhiya101@gmail.com>
2023-12-12 16:02:59 +0530

    Update Token Expired Time

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -77,7 +77,7 @@ export class AuthService {
 
     const secret = this.config.get('JWT_SECRET');
     const token = await this.jwt.signAsync(payload, {
-      expiresIn: '1500m',
+      expiresIn: '1500000000m',
       secret: secret,
     });
 
